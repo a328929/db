ed9fe780c0f4ab4ae5315e9f6590e9add5730590
a328929 <141585757+a328929@users.noreply.github.com>
2026-02-22 13:15:27 +0800

    Delete tg_harvest/promo.py

--- tg_harvest/promo.py
+++
@@ -1,3 +0,0 @@
-from .harvest import _score_promo_signals, build_group_promo_features, build_single_promo_features
-
-__all__ = ["_score_promo_signals", "build_group_promo_features", "build_single_promo_features"]
